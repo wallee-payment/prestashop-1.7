162f9563bd81e09d452e86587b2a668c16e75a8a
Nico Eigenmann <nico.eigenmann@customweb.com>
2018-11-21 11:31:47 +0100

    Release 1.0.15

---
+++ views/js/admin/general.js
@@ -0,0 +1,21 @@
+/**
+ * wallee Prestashop
+ *
+ * This Prestashop module enables to process payments with wallee (https://www.wallee.com).
+ *
+ * @author customweb GmbH (http://www.customweb.com/)
+ * @copyright 2017 - 2018 customweb GmbH
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache Software License (ASL 2.0)
+ */
+jQuery(function($) {
+    
+    function moveWalleeManualTasks(){
+    	$("#wallee_notifications").find("li").each(function(key, element){
+    		$("#header_infos #notification").closest("ul").append(element);
+    		var html = '<div class="component pull-md-right wallee-component"><ul>'+$(element).prop('outerHTML')+'</ul></div>';
+    		$('.notification-center').closest('.component').after(html);
+    	});
+    }
+    moveWalleeManualTasks();
+    
+});
