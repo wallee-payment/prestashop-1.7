e29d0e4b5d26eff0892ea635cab834fcaacd04e5
victoralbala <victor.albala@wallee.com>
2023-05-17 10:11:24 +0100

    Release 1.2.35

--- inc/Basemodule.php
+++ inc/Basemodule.php
@@ -150,7 +150,7 @@ class WalleeBasemodule
             $module->registerHook('displayAdminOrderMain') && $module->registerHook('displayAdminOrderTabLink') &&
             $module->registerHook('displayBackOfficeHeader') && $module->registerHook('displayOrderDetail') &&
             $module->registerHook('actionProductCancel') && $module->registerHook('walleeSettingsChanged') &&
-            $module->registerHook('actionOrderSlipAdd');
+            $module->registerHook('actionAfterUpdateCancelProductFormHandler');
     }
 
     public static function installConfigurationValues()
